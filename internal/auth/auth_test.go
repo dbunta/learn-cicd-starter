06addeac139d40076b316ddec695e4539bef93fb
dbunta <dmbunta@gmail.com>
2025-09-29 20:15:47 -0400

    fix broken test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -18,7 +18,7 @@ func TestGetAPIKey2(t *testing.T) {
 	req, _ := http.NewRequest("GET", "", nil)
 	req.Header.Add("Authorization", "test")
 	res, err := GetAPIKey(req.Header)
-	if err != nil || res != "" {
+	if err == nil || res != "" {
 		t.Errorf("expected error, but did not get one")
 	}
 }
